ce5b0f989edd876f39024f892949120b0bb03aaa
s02292 <s02292@contabile.fermi.intra>
2023-02-07 13:24:03 +0100

    modifiche al #1

--- index.php
+++ index.php
@@ -4,15 +4,51 @@ include "parameters.php";
 
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Home Page</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-  </head>
-  <body>
-    <div class="container text-center">
-        <div class="row">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Home Page</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    </head>
+    <body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+            </a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+            </li>
+        </ul>
+        <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+        </div>
+    </div>
+    </nav>
+    
+    
             <?php
 
             $connect = mysqli_connect($servername, $username, $password)
@@ -22,53 +58,21 @@ include "parameters.php";
                 or die("Impossibile selezione il database " . mysqli_error($connect));
 
             # Lista dei film
-            $query = "SELECT F.Titolo FROM 001_FILM as F";
+            $query = "SELECT F.Titolo, F.Regista, F.Genere FROM 001_FILM as F";
             $result = mysqli_query($connect, $query)
                 or die ("Errore nella query <br>" . mysqli_error($connect));
 
-            print('<div class="col">');
-            print('<ul class="list-group">');
-            print('<li class="list-group-item active" aria-current="true">Film</li>');
-           
             while($search = mysqli_fetch_array($result)){
-                print("<li class='list-group-item'><a href='#'>$search[Titolo]</a></li>");
-            }
-            print('</ul>');
-            print('</div>');
-            
-            # Lista degli attori
-            $query = "SELECT A.Nome FROM 001_ATTORI as A";
-            $result = mysqli_query($connect, $query)
-                or die ("Errore nella query <br>" . mysqli_error($connect));
-
-            print('<div class="col">');
-            print('<ul class="list-group">');
-            print('<li class="list-group-item active" aria-current="true">Attori</li>');
-           
-            while($search = mysqli_fetch_array($result)){
-                print("<li class='list-group-item'><a href='#'>$search[Nome]</a></li>");
-            }
-            print('</ul>');
-            print('</div>');
-
-            # Lista dei cinema
-            $query = "SELECT S.Nome, S.Citta FROM 001_SALE as S";
-            $result = mysqli_query($connect, $query)
-                or die ("Errore nella query <br>" . mysqli_error($connect));
-
-            print('<div class="col">');
-            print('<ul class="list-group">');
-            print('<li class="list-group-item active" aria-current="true">Cinema</li>');
-           
-            while($search = mysqli_fetch_array($result)){
-                print("<li class='list-group-item'><a href='#'> $search[Nome], $search[Citta] </a></li>");
-            }
-            print('</ul>');
-            print('</div>');
+                print('<div class="card text-bg-dark my-2">');
+                    print('<img src="images/fantozzi.jpg" class="card-img" alt="...">');
+                    print('<div class="card-img-overlay">');
+                        print("<h5 class='card-title'>$search[Titolo]</h5>");
+                        print("<p class='card-text'>$search[Regista]</p>");
+                        print("<p class='card-text'><small>$search[Genere]</small></p>");
+                    print("</div>");
+                print("</div>");
+            }   
             ?>
-
-    </div>
-        </div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
   </body>
