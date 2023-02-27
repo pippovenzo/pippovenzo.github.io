ec26404a1dabb1ad42f4342163643a8c3b87fdc2
s02292 <s7332919g@fermibassano.edu.it>
2023-02-27 18:24:02 +0100

    last changes

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include "parameters.php";
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Film</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-        <script src="script.js"></script> 
+        <link rel="icon" type="image/x-icon" href="/images/favicon.ico"> 
     </head>
     <body style="background-color:#323232;">
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
