addc8d2d04c1289061519bcedbffd3a025f79b19
s02292 <s7332919g@fermibassano.edu.it>
2023-03-06 21:12:23 +0100

    prova pwa

--- index.php
+++ index.php
@@ -10,8 +10,17 @@ include "parameters.php";
         <title>Film</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <link rel="icon" type="image/x-icon" href="/images/favicon.ico"> 
+        <link rel="manifest" href="manifest.json">
+        
     </head>
     <body style="background-color:#323232;">
+
+    <script>
+        if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register("serviceWorker.js");
+        }
+    </script>
+
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
         <a class="navbar-brand" href="index.php">Nuovo Cinema Paradiso</a>
@@ -38,6 +47,11 @@ include "parameters.php";
     
     <div class="row mx-5">
 
+    <script>
+        if ('serviceWorker' in navigator){
+            navigator.servicerWorker.register("serviceWorker.js");
+        }
+    </script>
     
     <?php
 
